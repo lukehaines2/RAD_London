9503b32ddcc8bdf4e40f175334b26b8b25e25587
Christine Ho <christine_ho@hotmail.co.uk>
2015-10-06 11:32:50 +0100

    markers inserted

--- public/js/map.js
+++ public/js/map.js
@@ -2,74 +2,42 @@
 // feature. People can enter geographical searches. The search box will return a
 // pick list containing a mix of places and predicted search terms.
 
-function initAutocomplete() {
+function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 51.50949, lng: -0.102654},
     zoom: 14,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     styles: styledArray
-  });
+  }) 
+  setMarkers(map);
+  }
 
-  // // Create the search box and link it to the UI element.
-  // var input = document.getElementById('pac-input');
-  // var searchBox = new google.maps.places.SearchBox(input);
-  // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  // // Bias the SearchBox results towards current map's viewport.
-  // map.addListener('bounds_changed', function() {
-  //   searchBox.setBounds(map.getBounds());
-  // });
-
-  // var markers = [];
-  // // [START region_getplaces]
-  // // Listen for the event fired when the user selects a prediction and retrieve
-  // // more details for that place.
-  // searchBox.addListener('places_changed', function() {
-  //   var places = searchBox.getPlaces();
-
-  //   if (places.length == 0) {
-  //     return;
-  //   }
-
-  //   // Clear out the old markers.
-  //   markers.forEach(function(marker) {
-  //     marker.setMap(null);
-  //   });
-  //   markers = [];
-
-  //   // For each place, get the icon, name and location.
-  //   var bounds = new google.maps.LatLngBounds();
-  //   places.forEach(function(place) {
-  //     var icon = {
-  //       url: place.icon,
-  //       size: new google.maps.Size(71, 71),
-  //       origin: new google.maps.Point(0, 0),
-  //       anchor: new google.maps.Point(17, 34),
-  //       scaledSize: new google.maps.Size(25, 25)
-  //     };
+var locations = [
+    ['Battle of Cable Street', 51.519184, -0.058872], 
+    ['Suffragettes in the EastEnd', 51.5144, 0.0586], 
+    ['The Blind Beggar', 51.519184, -0.058872], 
+    ['Bow Road Police Station', 51.533791, -0.024848]
+    ];
 
 
-  //     // Create a marker for each place.
-  //     markers.push(new google.maps.Marker({
-  //       map: map,
-  //       icon: icon,
-  //       title: place.name,
-  //       position: place.geometry.location
-  //     }));
-
-
-
-  //     if (place.geometry.viewport) {
-  //       // Only geocodes have viewport.
-  //       bounds.union(place.geometry.viewport);
-  //     } else {
-  //       bounds.extend(place.geometry.location);
-  //     }
-  //   });
-  //   map.fitBounds(bounds);
-  // });
-  // // [END region_getplaces]
+function setMarkers(map) {
+  console.log(locations)
+  for (var i = 0; i < locations.length; i++) {
+    var location = locations[i];
+    var marker = new google.maps.Marker({
+      position: {lat: location[1], lng: location[2]},
+      map: map,
+      title: location[0],
+      // zIndex: location[3]
+    });
+  }
 }
+
+
+
+
+
+
 
 
 
