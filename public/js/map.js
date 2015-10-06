655240016a7840d3dd411a30dfa7028a5e6f017d
Ambar Sethi <ambarrrsethi@gmail.com>
2015-10-06 20:42:06 +0100

    changed coordinates slightly, but seems not to update minor longitude changes

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,6 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 51.533791, lng: -0.024848},
+    center: {lat: 51.533746, lng: -0.024878},
     zoom: 13,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     styles: styledArray
