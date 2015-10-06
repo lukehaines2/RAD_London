e2fb1d2be1321002dacd20f3f2180fe274488a16
Christine Ho <christine_ho@hotmail.co.uk>
2015-10-06 11:08:42 +0100

    changes to markers and information boxes

--- public/js/map.js
+++ public/js/map.js
@@ -2,39 +2,67 @@
 // feature. People can enter geographical searches. The search box will return a
 // pick list containing a mix of places and predicted search terms.
 
-function initAutocomplete() {
-  var map = new google.maps.Map(document.getElementById('map'), {
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 51.50949, lng: -0.102654},
-    zoom: 12,
+    zoom: 15,
     mapTypeId: google.maps.MapTypeId.ROADMAP
-  });
+  
+});
 
   // Create the search box and link it to the UI element.
-  var input = document.getElementById('pac-input');
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // var input = document.getElementById('pac-input');
+  // var searchBox = new google.maps.places.SearchBox(input);
+  // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
+  // // Bias the SearchBox results towards current map's viewport.
+  // map.addListener('bounds_changed', function() {
+  //   searchBox.setBounds(map.getBounds());
+  // });
 
-  var markers = [];
+  var markers = [
+      ['Battle of Cable Street', 51.519184, -0.058872, 1], 
+      ['Cerial Killer', 51.5108, 0.0556, 2], 
+      ['The Blind Beggar', 51.519183, -0.058871, 3], 
+      ['Sylvia Pankhurst sets up East London suffragette group', 51.519184, -0.058872, 4]
+      ];
+
+      for (i = 0; i < markers.length; i++) {
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(markers[i][1], markers[i][2], markers[i][3], markers[i][4]), 
+        map: map
+      });
+
   // [START region_getplaces]
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
+  // searchBox.addListener('places_changed', function() {
+    // var places = searchBox.getPlaces();
 
-    if (places.length == 0) {
-      return;
-    }
+    // if (places.length == 0) {
+    //   return;
+    // }
 
     // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(places);
-    });
-    markers = [];
+    // markers.forEach(function(marker) {
+    //   marker.setMap(markers);
+    // });
+    // markers.forEach(function() {
+
+    // });
+  
+
+    var infowindow = new google.maps.InfoWindow();
+
+    // var marker, i; 
+
+    //   google.maps.event.addEventListener(marker, 'click', (function(marker, i) {
+    //     return function() {
+    //       infowindow.setContent(locations[i][0]);
+    //       infowindow.open(map, marker);
+    //     }
+    //   }) (marker, i));
+    // }
 
     // For each place, get the icon, name and location.
     var bounds = new google.maps.LatLngBounds();
@@ -48,46 +76,22 @@ function initAutocomplete() {
       };
 
       // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
+      // markers.push(new google.maps.Marker({
+      //   map: map,
+      //   icon: icon,
+      //   title: place.name,
+      //   position: place.geometry.location
+      // }));
 
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-  });
+    //   if (place.geometry.viewport) {
+    //     // Only geocodes have viewport.
+    //     bounds.union(place.geometry.viewport);
+    //   } else {
+    //     bounds.extend(place.geometry.location);
+    //   }
+    // });
+    // map.fitBounds(bounds);
+  // };
   // [END region_getplaces]
 }
 
-// var marker;
-
-// function initMap() {
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     zoom: 13,
-//     center: {lat: 59.325, lng: 18.070}
-//   });
-
-//   marker = new google.maps.Marker({
-//     map: map,
-//     draggable: true,
-//     animation: google.maps.Animation.DROP,
-//     position: {lat: 59.327, lng: 18.067}
-//   });
-//   marker.addListener('click', toggleBounce);
-// }
-
-// function toggleBounce() {
-//   if (marker.getAnimation() !== null) {
-//     marker.setAnimation(null);
-//   } else {
-//     marker.setAnimation(google.maps.Animation.BOUNCE);
-//   }
-// }
